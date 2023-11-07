01bff8686504114c1c78c41399e1e842d97447d2
Jacob Young <jacob@jry.io>
2023-11-07 13:24:49 -0500

    Remove URL token parsing

--- static/app-sdk.js
+++ static/app-sdk.js
@@ -1,28 +1,9 @@
 // index.ts
 var SDK_LOADED_START = "sdkLoadedStart";
 var SDK_LOADED_END = "sdkLoadedEnd";
-var DEV_TOKEN_PROMPT = "Enter your Recurse Personal Access Token (https://www.recurse.com/settings/apps)";
-var DEV_TOKEN_MISSING = "RCTV SDK loaded without an RC access token. This is probably our fault. Send a Zulip message to either @Greg Sadetsky or @Jacob Young";
-var getToken = () => {
-  let token = null;
-  const queryString = window.location.search;
-  if (!queryString) {
-    do {
-      token = window.prompt(DEV_TOKEN_PROMPT);
-    } while (!token);
-    return token;
-  }
-  const urlParams = new URLSearchParams(queryString);
-  token = urlParams.get("rcToken");
-  if (!token) {
-    alert(DEV_TOKEN_MISSING);
-    return "";
-  }
-  window.history.replaceState({}, document.title, window.location.pathname);
-  return token;
+var getSignedInUsers = (token2) => {
 };
 var onLoad = (theirs) => {
-  const token = getToken();
   window.parent.postMessage({
     type: SDK_LOADED_END
   }, "*");
@@ -30,8 +11,6 @@ var onLoad = (theirs) => {
     getSignedInUsers: () => getSignedInUsers(token)
   };
   theirs(API);
-};
-var getSignedInUsers = (token) => {
 };
 document.addEventListener("DOMContentLoaded", () => {
   window.parent.postMessage({
