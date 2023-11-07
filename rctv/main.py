263e86ce01bfb0d00ac8e0eaba113a3c38c6e713
Jacob Young <jacob@jry.io>
2023-11-07 16:59:43 -0500

    Some basic calendar json stuff

--- rctv/main.py
+++ rctv/main.py
@@ -3,20 +3,36 @@ import os
 import secrets
 from pathlib import Path
 from typing import Annotated
+from datetime import datetime, timedelta
+
+import recurring_ical_events
 
 from dotenv import load_dotenv
-from fastapi import Depends, FastAPI, HTTPException, Request, Response, status
+from fastapi import (
+    Depends,
+    FastAPI,
+    HTTPException,
+    Request,
+    Response,
+    status,
+    BackgroundTasks,
+)
 from fastapi.staticfiles import StaticFiles
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.status import HTTP_400_BAD_REQUEST
 
+from .calendar_api import get_events
 from .recurse_api import get_hub_visits_for_today, get_profile
 
 load_dotenv()
 
 META_REFRESH_SECONDS = 60
+TIME_FMT = "%Y%m%d"
+YEAR = 365
 RC_ACCESS_TOKEN = os.environ["RC_ACCESS_TOKEN"]
+RC_CALENDAR_URL = os.environ["RC_CALENDAR_URL"]
 BASIC_HTTP_AUTH_USER, BASIC_HTTP_AUTH_PASSWORD = os.environ["BASIC_HTTP_AUTH"].split(
     ":"
 )
@@ -26,6 +42,8 @@ DEBUG = os.environ.get("DEBUG") == "True"
 if not DEBUG:
     # do a test access using the rc api token, and fail immediately if it doesn't work
     assert get_profile(RC_ACCESS_TOKEN)["id"]
+    # test the calendar URL directly and cache the result
+    assert get_events(RC_CALENDAR_URL)
 
 app = FastAPI()
 templates = Jinja2Templates(directory="rctv/templates")
@@ -92,3 +110,65 @@ async def rctv(
             "rc_payload": rc_payload,
         },
     )
+
+
+@app.get("/api/events")
+async def cal(
+    _request: Request,
+    response: Response,
+    background_tasks: BackgroundTasks,
+    start: str | None = None,
+    end: str | None = None,
+):
+    """
+    Takes an optional query paramters ?start=YYYY-MM-DD and &end=YYYY-MM-DD
+    Returns calendar events as JSON between [start, end] date ranges
+    """
+
+    # This either returns the cached calendar or makes an API call
+    # This isn't ideal because if some RCTV is not regularly hitting this API we
+    # are going to get cache-misses and do the full API request which comes out
+    # to be 1.2 MB
+    #
+    # An alternative approach is to use something like fastapi_utils.repeat_every
+    # to run this on a periodic basis instead:
+    #
+    # https://fastapi-utils.davidmontague.xyz/user-guide/repeated-tasks/
+    cal = get_events(RC_CALENDAR_URL)
+
+    try:
+        start_datetime = (
+            datetime.strptime(start, TIME_FMT) if start is not None else datetime.now()
+        )
+        end_datetime = (
+            datetime.strptime(end, TIME_FMT)
+            if end is not None
+            else start_datetime + timedelta(days=(2 * YEAR))
+        )
+    except ValueError:
+        response.status_code = HTTP_400_BAD_REQUEST
+        return {
+            "message": "'start' and 'end' params must be formatted in %Y%m%d. Example: 20250101"
+        }
+
+    events = recurring_ical_events.of(cal)
+    events = events.between(
+        start_datetime.strftime(TIME_FMT), end_datetime.strftime(TIME_FMT)
+    )
+    j = []
+    for event in events:
+        if "SUMMARY" in event:
+            if "STATUS" in event and event["STATUS"] == "CANCELLED":
+                continue
+            e = {}
+            e["summary"] = event["SUMMARY"]
+            e["start"] = event["DTSTART"].dt
+            e["end"] = event["DTEND"].dt
+            duration = event["DTEND"].dt - event["DTSTART"].dt
+            e["duration_seconds"] = duration
+            e["duration_string"] = str(duration)
+            e["description"] = event["DESCRIPTION"]
+            e["location"] = event["LOCATION"]
+            j.append(e)
+
+    return j
